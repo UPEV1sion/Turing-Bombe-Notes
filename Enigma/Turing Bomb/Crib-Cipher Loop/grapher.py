980d31e9f22ecc4c948b7bb9780d163e03aaa5dc
UPEV1sion <130445385+UPEV1sion@users.noreply.github.com>
2024-10-04 11:50:19 +0200

    Added Runtime Graph of Cycle finding

---
+++ Enigma/Turing Bomb/Crib-Cipher Loop/grapher.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+with open("data.txt") as file:
+    lines = file.read().splitlines()
+    fig, ax = plt.subplots()
+    line = lines[0]
+    
+    y_axis_graph = []
+    y_axis_force = []
+    
+    for line in lines:
+        split_line = line.split(", ")
+        graph_num = float(split_line[0])
+        force_num = float(split_line[1])
+        y_axis_graph.append(graph_num)
+        y_axis_force.append(force_num)
+        
+    x_axis = [i + 2 for i in range(len(y_axis_graph))]
+    
+    ax.plot(x_axis, y_axis_graph, label="Graph approach")
+    ax.plot(x_axis, y_axis_force, label="\"Force\" approach")
+    ax.set_xlabel("Crib/Ciphertext length")
+    ax.set_ylabel("Runtime (seconds)")
+    ax.set_title("Runtime Graph vs Force (1000 passes per length)")
+    ax.legend()
+    ax.grid()
+    
+    plt.show()
--- Enigma/Turing Bomb/Cycle joke/find_cycle_ioccc.c
+++ Enigma/Turing Bomb/Cycle joke/find_cycle_ioccc.c
@@ -1,5 +1,5 @@
-static bool find_cycle(const uint8_t s,const uint8_t c,Tuple*t,const size_t n,
-uint32_t m,int8_t*cp,size_t*i,int8_t*p){cp[(*i)++]=c;if(c==s&&*i>1)return 1;
-for(size_t j=0;j<n;++j)if(!(m&(1<<j))){const uint8_t nl=(t[j].first==c)?t[j].
-second:(t[j].second==c)?t[j].first:0;if(nl){m|=(1<<j);p[*i-1]=j;if(find_cycle(
-s,nl,t,n,m,cp,i,p))return 1;m&=~(1<<j);p[*i-1]=-1;}}(*i)--;return 0;}
+static bool find_cycle(const uint8_t s, const uint8_t c, CharTuple*t, const uint8_t n, uint32_t v, CycleOld*y)
+{y->chars_w_stubs[y->len_w_stubs]=c;y->chars_wo_stubs[y->len_wo_stubs]=c;if(c==s&&y->len_w_stubs>1)return 1;
+for(uint8_t i=0;i<n;++i){if(!(v&(1<<i))){const uint8_t z=(t[i].first==c?t[i].second:(t[i].second==c?t[i].first:0));
+if(z){v|=(1<<i);y->positions_w_stubs[++y->len_w_stubs]=i;y->positions_wo_stubs[++y->len_wo_stubs]=i;if(f(s,z,t,n,v,y))
+return 1;v&=~(1<<i);}}}y->len_wo_stubs--;return 0;}
