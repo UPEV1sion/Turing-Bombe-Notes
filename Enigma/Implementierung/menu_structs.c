99e1731eefe46ae486e96c9caab9a5a62c99323f
UPE_V1sion <e.schaeffer82@gmail.com>
2024-11-12 22:27:04 +0100

    Reorganized files

---
+++ Enigma/Implementierung/menu_structs.c
@@ -0,0 +1,17 @@
+typedef struct MenuNode MenuNode;
+typedef struct CribCipherTuple CribCipherTuple;
+
+struct MenuNode
+{
+	CribCipherTuple *stubs;
+	uint8_t num_stubs;
+	char letter;
+};
+
+struct CribCipherTuple
+{
+	MenuNode first;
+	MenuNode second;
+	uint8_t position;
+	bool visited;
+};
