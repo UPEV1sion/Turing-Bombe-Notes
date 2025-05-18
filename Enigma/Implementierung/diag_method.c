26047ea77af3c6fb614c6468462184c8c61a158a
Emanuel Schaeffer <emanuel.schaeffer@proton.me>
2025-05-18 12:12:07 +0200

    finished tufte book

--- Enigma/Implementierung/diag_method.c
+++ Enigma/Implementierung/diag_method.c
@@ -1,12 +1,17 @@
-static void activate_contact(TuringBombe *turing_bombe, 
-							 uint8_t first_contact, 
-							 uint8_t second_contact)
+static void activate_contact(
+				TuringBombe *turing_bombe, 
+				uint8_t first_contact, 
+				uint8_t second_contact)
 {
-	Contact *primary_contact = turing_bombe->terminal->contacts[first_contact];
-	Contact *secondary_contact = turing_bombe->terminal->contacts[second_contact];
+	Contact *primary_contact = 
+		turing_bombe->terminal->contacts[first_contact];
+	Contact *secondary_contact = 
+		turing_bombe->terminal->contacts[second_contact];
 
-	primary_contact->active_cable_connections[primary_contact->num_active_connections] = second_contact;
-	secondary_contact->active_cable_connections[secondary_contact->num_active_connections] = first_contact;
+	primary_contact->active_cable_connections[
+		primary_contact->num_active_connections] = second_contact;
+	secondary_contact->active_cable_connections[
+		secondary_contact->num_active_connections] = first_contact;
 
 	primary_contact->active_bit_vector |= (1 << second_contact);
 	secondary_contact->active_bit_vector |= (1 << first_contact);
